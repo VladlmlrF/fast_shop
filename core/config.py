187daa9ebd5eee80b9a26c10cc0810495c61fb55
VladlmlrF <vladimirfalin84@gmail.com>
2023-12-07 14:25:11 +0700

    create core.config

---
+++ core/config.py
@@ -0,0 +1,20 @@
+from pydantic_settings import BaseSettings
+from pydantic_settings import SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    DB_HOST: str
+    DB_PORT: int
+    DB_NAME: str
+    DB_USER: str
+    DB_PASS: str
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+
+settings = Settings()
+
+DATABASE_URL = (
+    f"postgresql+asyncpg://{settings.DB_USER}:"
+    f"{settings.DB_PASS}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
+)
