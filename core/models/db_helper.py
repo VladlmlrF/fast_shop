88870932524f350825c37dc850c1cffc7160f779
VladlmlrF <vladimirfalin84@gmail.com>
2023-12-07 20:18:25 +0700

    create func scoped_session_dependency in db_helper.py

--- core/models/db_helper.py
+++ core/models/db_helper.py
@@ -1,3 +1,6 @@
+from asyncio import current_task
+
+from sqlalchemy.ext.asyncio import async_scoped_session
 from sqlalchemy.ext.asyncio import async_sessionmaker
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.ext.asyncio import create_async_engine
@@ -8,14 +11,21 @@ from core.config import DATABASE_URL
 class DatabaseHelper:
     def __init__(self, url: str, echo: bool = False):
         self.engine = create_async_engine(url=url, echo=echo)
-        self.session = async_sessionmaker(
+        self.session_factory = async_sessionmaker(
             bind=self.engine, autoflush=False, autocommit=False, expire_on_commit=False
         )
 
-    async def session_dependency(self) -> AsyncSession:
-        async with self.session as ses:
-            yield ses
-            ses.close()
+    def get_scoped_session(self):
+        session = async_scoped_session(
+            session_factory=self.session_factory,
+            scopefunc=current_task,
+        )
+        return session
+
+    async def scoped_session_dependency(self) -> AsyncSession:
+        session = self.get_scoped_session()
+        yield session
+        await session.close()
 
 
 db_helper = DatabaseHelper(url=DATABASE_URL, echo=False)
