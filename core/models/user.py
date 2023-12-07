822619d7cff29e7d5498e82b8d4571c9973af074
VladlmlrF <vladimirfalin84@gmail.com>
2023-12-07 14:38:37 +0700

    create model User

---
+++ core/models/user.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Boolean
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+
+from .base import Base
+
+
+class User(Base):
+    username: Mapped[str] = mapped_column(String(40), unique=True)
+    email: Mapped[str] = mapped_column(String(100), unique=True)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
