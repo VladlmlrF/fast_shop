2e30371522a5f282dc06d56c4c59a5ce513aa8a5
VladlmlrF <vladimirfalin84@gmail.com>
2023-12-09 22:21:45 +0700

    create PASSWORD_VALIDATOR in core.config.py

--- core/config.py
+++ core/config.py
@@ -1,5 +1,9 @@
+from pathlib import Path
+
 from pydantic_settings import BaseSettings
 from pydantic_settings import SettingsConfigDict
+
+BASE_DIR = Path(__file__).parent.parent
 
 
 class Settings(BaseSettings):
@@ -17,4 +21,8 @@ settings = Settings()
 DATABASE_URL = (
     f"postgresql+asyncpg://{settings.DB_USER}:"
     f"{settings.DB_PASS}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
+)
+
+PASSWORD_VALIDATOR = (
+    "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{6,20}$"
 )
