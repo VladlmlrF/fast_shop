8ce5222e3585aaee0859bfaa58df6dfd21dd7c9d
VladlmlrF <vladimirfalin84@gmail.com>
2023-12-09 22:23:03 +0700

    add role, add field_validator in UserCreateSchema

--- users/schemas.py
+++ users/schemas.py
@@ -1,9 +1,14 @@
+import re
+
 from fastapi import HTTPException
 from fastapi import status
 from pydantic import BaseModel
 from pydantic import ConfigDict
 from pydantic import EmailStr
 from pydantic import field_validator
+
+from core.config import PASSWORD_VALIDATOR
+from core.models import Role
 
 
 class UserBaseSchema(BaseModel):
@@ -33,9 +38,22 @@ class UserUpdatePartialSchema(UserBaseSchema):
 class UserSchema(UserBaseSchema):
     id: int
     is_active: bool
+    role: Role
 
     model_config = ConfigDict(from_attributes=True)
 
 
 class UserCreateSchema(UserBaseSchema):
     password: str
+
+    @field_validator("password")
+    def validate_password(cls, value):
+        validator = re.search(re.compile(PASSWORD_VALIDATOR), value)
+        if validator:
+            return value
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail="The password length should be between 6 and 20 characters. "
+            "The password must include at least one digit, at least one uppercase and "
+            "one lowercase character and at least one special character.",
+        )
