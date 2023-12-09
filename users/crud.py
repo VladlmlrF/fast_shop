69159b6ed7565ae32e9d4e97c6ad052d222dc908
VladlmlrF <vladimirfalin84@gmail.com>
2023-12-09 12:18:44 +0700

    in crud.py create get_users, get_user, update_user, delete_user

--- users/crud.py
+++ users/crud.py
@@ -1,8 +1,12 @@
+from sqlalchemy import select
+from sqlalchemy.engine import Result
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from auth.utils import get_password_hash
 from core.models import User
 from users.schemas import UserCreateSchema
+from users.schemas import UserUpdatePartialSchema
+from users.schemas import UserUpdateSchema
 
 
 async def create_user(session: AsyncSession, user: UserCreateSchema) -> User:
@@ -15,3 +19,35 @@ async def create_user(session: AsyncSession, user: UserCreateSchema) -> User:
     session.add(new_user)
     await session.commit()
     return new_user
+
+
+async def get_users(session: AsyncSession) -> list[User]:
+    """Get all users"""
+    statement = select(User).order_by(User.id)
+    result: Result = await session.execute(statement=statement)
+    users = result.scalars().all()
+    return list(users)
+
+
+async def get_user(session: AsyncSession, user_id: int) -> User | None:
+    """Get user by user_id"""
+    return await session.get(User, user_id)
+
+
+async def update_user(
+    session: AsyncSession,
+    user: User,
+    user_update: UserUpdateSchema | UserUpdatePartialSchema,
+    partial: bool = False,
+) -> User:
+    """Update user"""
+    for name, value in user_update.model_dump(exclude_unset=partial).items():
+        setattr(user, name, value)
+    await session.commit()
+    return user
+
+
+async def delete_user(session: AsyncSession, user: User) -> None:
+    """Delete user"""
+    await session.delete(user)
+    await session.commit()
