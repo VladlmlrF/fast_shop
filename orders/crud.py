d637c0cc9a1bb8fa272bc492e8fa9d7596c07716
VladlmlrF <vladimirfalin84@gmail.com>
2023-12-20 22:11:40 +0700

    create get_card_by_user_id in orders.crud

--- orders/crud.py
+++ orders/crud.py
@@ -1,7 +1,4 @@
-# from fastapi import HTTPException, Depends
-# from fastapi import status
-# from sqlalchemy import select
-# from sqlalchemy.engine import Result
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.models import Cart
@@ -10,7 +7,7 @@ from orders.schemas import CartItemCreateSchema
 from products.crud import get_product
 
 
-async def create_cart(session: AsyncSession, user_id: int):
+async def create_cart(session: AsyncSession, user_id: int) -> Cart | None:
     """Create cart"""
     cart = Cart(user_id=user_id)
     session.add(cart)
@@ -21,6 +18,13 @@ async def create_cart(session: AsyncSession, user_id: int):
 async def get_cart(session: AsyncSession, cart_id: int) -> Cart | None:
     """Get cart by id"""
     return await session.get(Cart, cart_id)
+
+
+async def get_card_by_user_id(session: AsyncSession, user_id: int) -> Cart | None:
+    """Get cart by user.id"""
+    statement = select(Cart).where(Cart.user_id == user_id)
+    cart: Cart | None = await session.scalar(statement)
+    return cart
 
 
 async def create_cart_item(
