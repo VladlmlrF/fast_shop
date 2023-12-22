eabbf0fcf51456696b17b77e005e0d3c510303cb
VladlmlrF <vladimirfalin84@gmail.com>
2023-12-22 22:33:25 +0700

    create get_cart_item and update_cart_item in orders.views

--- orders/views.py
+++ orders/views.py
@@ -5,6 +5,8 @@ from fastapi import status
 
 from . import crud
 from .schemas import CartItemCreateSchema
+from .schemas import CartItemSchema
+from .schemas import CartItemUpdateSchema
 from .schemas import CartSchema
 from auth.utils import get_current_user_name
 from auth.utils import get_user_by_username
@@ -62,3 +64,46 @@ async def cart_add_item(
             return cart
         raise cart_not_found_exc
     raise cart_not_found_exc
+
+
+@router.get("/cart_item/{cart_item_id}", response_model=CartItemSchema)
+async def get_cart_item(
+    cart_item_id: int,
+    session=Depends(db_helper.scoped_session_dependency),
+    current_user_name: str = Depends(get_current_user_name),
+):
+    not_found_exc = HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"Cart item {cart_item_id} not found!",
+    )
+    if current_user_name:
+        cart_item = await crud.get_cart_item_by_id(
+            session=session, cart_item_id=cart_item_id
+        )
+        if cart_item:
+            return cart_item
+        raise not_found_exc
+    raise not_found_exc
+
+
+@router.patch("/cart_item/{cart_item_id}", response_model=CartItemSchema)
+async def update_cart_item(
+    cart_item_update: CartItemUpdateSchema,
+    cart_item_id: int,
+    session=Depends(db_helper.scoped_session_dependency),
+    current_user_name: str = Depends(get_current_user_name),
+):
+    not_found_exc = HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"Cart item {cart_item_id} not found!",
+    )
+    if current_user_name:
+        cart_item = await crud.get_cart_item_by_id(
+            session=session, cart_item_id=cart_item_id
+        )
+        if cart_item:
+            return await crud.update_cart_item(
+                session=session, cart_item=cart_item, cart_item_update=cart_item_update
+            )
+        raise not_found_exc
+    raise not_found_exc
