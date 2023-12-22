c0c251b310d02a60d35608837873ccbf57f2fbf1
VladlmlrF <vladimirfalin84@gmail.com>
2023-12-22 17:56:18 +0700

    refactor webapp.router.py

--- webapp/router.py
+++ webapp/router.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 from fastapi import Depends
+from fastapi import HTTPException
 from fastapi import Request
 from fastapi import status
 from fastapi.responses import HTMLResponse
@@ -11,7 +12,10 @@ from auth.utils import get_user_by_username
 from core.models import db_helper
 from core.models import User
 from orders.crud import create_cart
+from orders.crud import create_cart_item_with_one_product
 from orders.crud import get_card_by_user_id
+from orders.crud import get_cart_items_by_cart_id
+from products.crud import get_product
 from products.crud import get_product_by_name
 from products.crud import get_products
 
@@ -63,7 +67,66 @@ async def get_cart(
     cart = await get_card_by_user_id(session=session, user_id=current_user.id)
     if not cart:
         cart = await create_cart(session=session, user_id=current_user.id)
+    items = await get_cart_items_by_cart_id(session=session, cart_id=cart.id)
+    products = []
+    total_cost: int = 0
+    for item in items:
+        product = await get_product(session=session, product_id=item.product_id)
+        quantity: int = item.quantity
+        products.append({product: quantity})
+        item_cost = product.price * quantity
+        total_cost += item_cost
     return templates.TemplateResponse(
         "cart.html",
-        {"request": request, "cart": cart, "current_user_name": current_user_name},
+        {
+            "request": request,
+            "cart": cart,
+            "current_user_name": current_user_name,
+            "products": products,
+            "total_cost": total_cost,
+        },
     )
+
+
+@router.get("/cart/{product_id}", response_class=HTMLResponse)
+async def get_cart(
+    request: Request,
+    product_id: int,
+    session=Depends(db_helper.scoped_session_dependency),
+    current_user_name: str = Depends(get_current_user_name),
+):
+    current_user: User | None = await get_user_by_username(
+        session=session, username=current_user_name
+    )
+    cart = await get_card_by_user_id(session=session, user_id=current_user.id)
+    if not cart:
+        cart = await create_cart(session=session, user_id=current_user.id)
+    try:
+        await create_cart_item_with_one_product(
+            session=session, cart_id=cart.id, product_id=product_id
+        )
+        items = await get_cart_items_by_cart_id(session=session, cart_id=cart.id)
+        products = []
+        total_cost: int = 0
+        for item in items:
+            product = await get_product(session=session, product_id=item.product_id)
+            quantity: int = item.quantity
+            products.append({product: quantity})
+            item_cost = product.price * quantity
+            total_cost += item_cost
+        return templates.TemplateResponse(
+            "cart.html",
+            {
+                "request": request,
+                "cart": cart,
+                "current_user_name": current_user_name,
+                "products": products,
+                "total_cost": total_cost,
+            },
+        )
+    except HTTPException:
+        return templates.TemplateResponse(
+            name="404.html",
+            context={"request": request},
+            status_code=status.HTTP_404_NOT_FOUND,
+        )
