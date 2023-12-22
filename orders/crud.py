ea0854a66c3f83a12a60bbc74cb3537acf3d56df
VladlmlrF <vladimirfalin84@gmail.com>
2023-12-22 17:54:41 +0700

    create create_cart_item_with_one_product and get_cart_items_by_cart_id in orders.crud.py

--- orders/crud.py
+++ orders/crud.py
@@ -1,3 +1,5 @@
+from fastapi import HTTPException
+from fastapi import status
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -70,3 +72,41 @@ async def create_cart_item(
     session.add(new_cart_item)
     await session.commit()
     return new_cart_item
+
+
+async def create_cart_item_with_one_product(
+    session: AsyncSession, cart_id: int, product_id: int, quantity: int = 1
+):
+    """Create cart_item"""
+    product = await get_product(session=session, product_id=product_id)
+    if product:
+        price = product.price
+        old_cart_item = await get_cart_item_by_cart_id_and_product_id(
+            session=session, cart_id=cart_id, product_id=product_id
+        )
+        if old_cart_item:
+            old_cart_item.quantity += quantity
+            await session.commit()
+            return old_cart_item
+
+        new_cart_item = CartItem(
+            cart_id=cart_id,
+            product_id=product_id,
+            price=price,
+            quantity=quantity,
+        )
+        session.add(new_cart_item)
+        await session.commit()
+        return new_cart_item
+    raise HTTPException(
+        status_code=status.HTTP_403_FORBIDDEN, detail=f"Product {product_id} not found"
+    )
+
+
+async def get_cart_items_by_cart_id(
+    session: AsyncSession,
+    cart_id: int,
+):
+    statement = select(CartItem).where(CartItem.cart_id == cart_id)
+    items: list[CartItem] = list(await session.scalars(statement))
+    return items
