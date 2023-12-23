3712c9ce901bedc4b17fd805d8f27292c9e4015a
VladlmlrF <vladimirfalin84@gmail.com>
2023-12-23 18:50:04 +0700

    create update_cart in webapp.router

--- webapp/router.py
+++ webapp/router.py
@@ -7,6 +7,7 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
 from .auth.router import router as auth_router
+from .forms import CartUpdateForm
 from auth.utils import get_current_user_name
 from auth.utils import get_user_by_username
 from core.models import db_helper
@@ -15,6 +16,8 @@ from orders.crud import create_cart
 from orders.crud import create_cart_item_with_one_product
 from orders.crud import get_card_by_user_id
 from orders.crud import get_cart_items_by_cart_id
+from orders.crud import update_cart_item
+from orders.schemas import CartItemUpdateSchema
 from products.crud import get_product
 from products.crud import get_product_by_name
 from products.crud import get_products
@@ -130,3 +133,52 @@ async def get_cart(
             context={"request": request},
             status_code=status.HTTP_404_NOT_FOUND,
         )
+
+
+@router.get("/update_cart/", response_class=HTMLResponse)
+async def update_cart(
+    request: Request,
+    session=Depends(db_helper.scoped_session_dependency),
+    current_user_name: str = Depends(get_current_user_name),
+):
+    current_user: User | None = await get_user_by_username(
+        session=session, username=current_user_name
+    )
+    cart = await get_card_by_user_id(session=session, user_id=current_user.id)
+    items = await get_cart_items_by_cart_id(session=session, cart_id=cart.id)
+    products = []
+    for item in items:
+        product = await get_product(session=session, product_id=item.product_id)
+        products.append({product.name: [item.quantity]})
+    return templates.TemplateResponse(
+        "update_cart.html", {"request": request, "products": products}
+    )
+
+
+@router.post("/update_cart/", response_class=HTMLResponse)
+async def update_cart(
+    request: Request,
+    session=Depends(db_helper.scoped_session_dependency),
+    current_user_name: str = Depends(get_current_user_name),
+):
+    current_user: User | None = await get_user_by_username(
+        session=session, username=current_user_name
+    )
+    cart = await get_card_by_user_id(session=session, user_id=current_user.id)
+    items = await get_cart_items_by_cart_id(session=session, cart_id=cart.id)
+    products = []
+    for item in items:
+        product = await get_product(session=session, product_id=item.product_id)
+        form = CartUpdateForm(request=request)
+        await form.load_data()
+        quantity = item.quantity
+        new_quantity = int(form.quantity) if form.quantity else item.quantity
+        products.append({product.name: [quantity, new_quantity]})
+        await update_cart_item(
+            session=session,
+            cart_item=item,
+            cart_item_update=CartItemUpdateSchema(quantity=new_quantity),
+        )
+    return templates.TemplateResponse(
+        "update_cart.html", {"request": request, "products": products}
+    )
