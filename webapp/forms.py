f6ade16a64ec4877fcd7f4ab886ae693efb42f1f
VladlmlrF <vladimirfalin84@gmail.com>
2023-12-23 18:48:31 +0700

    create CartUpdateForm

---
+++ webapp/forms.py
@@ -0,0 +1,11 @@
+from fastapi import Request
+
+
+class CartUpdateForm:
+    def __init__(self, request: Request):
+        self.request: Request = request
+        self.quantity: int | None = None
+
+    async def load_data(self):
+        form = await self.request.form()
+        self.quantity = form.get("quantity")
