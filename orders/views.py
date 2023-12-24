dd4d3791e00b02beaa900e599cf2a7a02a79a223
VladlmlrF <vladimirfalin84@gmail.com>
2023-12-24 23:08:34 +0700

    create get_orders and get_order in orders.views

--- orders/views.py
+++ orders/views.py
@@ -33,3 +33,34 @@ async def create_order(
     raise HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED, detail="You need to log in"
     )
+
+
+@router.get("/", response_model=list[OrderSchema])
+async def get_orders(
+    current_user_name: str = Depends(get_current_user_name),
+    session=Depends(db_helper.scoped_session_dependency),
+):
+    current_user: User | None = await get_user_by_username(
+        session=session, username=current_user_name
+    )
+    if current_user.is_super_admin or current_user.is_admin:
+        return await crud.get_orders(session=session)
+    raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Forbidden")
+
+
+@router.get("/{order_id}", response_model=OrderSchema)
+async def get_order(
+    order_id: int,
+    current_user_name: str = Depends(get_current_user_name),
+    session=Depends(db_helper.scoped_session_dependency),
+):
+    if current_user_name:
+        order = await crud.get_order_by_id(session=session, order_id=order_id)
+        if order:
+            return order
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=f"Order {order_id} not found!"
+        )
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED, detail="You need to log in"
+    )
